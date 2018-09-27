8f429ab5545538033ebd5079a97194520fb5e04a
Daniel Roger Hansen <daniel.brann@hotmail.no>
2018-09-27 13:45:19 +0200

    Nailed it bitches

---
+++ obligatoriskOppgave1/src/obligatoriskoppgave1/ObligatoriskOppgave1.java
@@ -0,0 +1,10 @@
+package obligatoriskoppgave1;
+
+public class ObligatoriskOppgave1 {
+    public static void main(String[] args) {
+        System.out.println("Obligatorisk oppgave 1" + '\n' + 
+                           "Daniel Roger Hansen - s333748" + '\n' +
+                           "Åsmund Gundersen - s330362" + '\n' + 
+                           "Herman Vognild Rustad - s333769");  
+    } 
+}
